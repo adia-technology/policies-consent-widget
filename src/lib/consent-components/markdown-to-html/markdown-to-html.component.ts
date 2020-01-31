92eafce8548b3dec11bdbd31d09f1e91008ac2b0
Michał Pogorzelski <mpogorzelski@future-processing.com>
2020-01-31 10:31:16 +0100

    Css styling fix, travis configuration

--- src/lib/consent-components/markdown-to-html/markdown-to-html.component.ts
+++ src/lib/consent-components/markdown-to-html/markdown-to-html.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-markdown-to-html',
   templateUrl: './markdown-to-html.component.html',
-  styleUrls: ['./markdown-to-html.component.scss']
+  styleUrls: ['./markdown-to-html.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MarkdownToHtmlComponent {
   @Input() markdownContent: string;
