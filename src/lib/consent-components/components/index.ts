352a5585b4f76705b1e2f45f05269ef6c8f16ad7
Piotr Szperka <pszperka@future-processing.com>
2020-04-10 16:29:45 +0200

    Workaround https://github.com/ng-packagr/ng-packagr/issues/195

--- src/lib/consent-components/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./markdown-to-html/markdown-to-html.component";
-export * from "./consent-switch/consent-switch.component";
-export * from "./consent-modal-navigation/consent-modal-navigation.component";
-export * from "./consent-modal/consent-modal.component";
-export * from "./consent-button/consent-button.component";
